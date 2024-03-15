2bece2a78302d184f8b166c1981a3f7773109699
Mykhailo Kushchov <mkuschov@gmail.com>
2024-03-15 17:28:22 -0400

    Implement launches api

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,14 @@
-import { Router } from "./deps.ts";
+import { Router, send } from "./deps.ts";
 import * as planets from "./models/planets.ts";
+import * as launches from "./models/launches.ts";
 
 const router = new Router();
 
-router.get("/", (ctx) => {
+router.get("/", async (ctx) => {
+  await send(ctx, "index.html", { root: `${Deno.cwd()}/public/` });
+});
+
+router.get("/ascii", (ctx) => {
   ctx.response.body = `
       {___     {__      {_         {__ __        {_       
       {_ {__   {__     {_ __     {__    {__     {_ __     
@@ -17,6 +22,35 @@ router.get("/", (ctx) => {
 
 router.get("/planets", (ctx) => {
   ctx.response.body = planets.getAll();
+});
+
+router.get("/launches", (ctx) => {
+  ctx.response.body = launches.getAll();
+});
+
+router.get("/launches/:id", (ctx) => {
+  if (ctx.params?.id) {
+    const launchesList = launches.getOne(Number(ctx.params.id));
+    if (launchesList) ctx.response.body = launchesList;
+    else {
+      ctx.throw(400, "Launch with this ID does not exist");
+    }
+  }
+});
+
+router.delete("/launches/:id", (ctx) => {
+  if (ctx.params?.id) {
+    const result = launches.removeOne(Number(ctx.params.id));
+    ctx.response.body = { success: result };
+  }
+});
+
+router.post("/launches", async (ctx) => {
+  const body = await ctx.request.body.json();
+
+  launches.addOne(body);
+  ctx.response.body = { success: true };
+  ctx.response.status = 201;
 });
 
 export default router;
