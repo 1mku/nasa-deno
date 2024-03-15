5a925e08ac396ebf18c12f515afec33229a9be1c
Mykhailo Kushchov <mkuschov@gmail.com>
2024-03-14 22:25:33 -0400

    Add router

--- main.ts
+++ main.ts
@@ -1,22 +1,40 @@
-import { Application } from "jsr:@oak/oak@14";
+import { Application, send } from "jsr:@oak/oak@14";
+import api from "./api.ts";
 
 const app = new Application();
 const PORT = 8000;
 
 app.use(async (ctx, next) => {
-  ctx.response.body = `
-    {___     {__      {_         {__ __        {_       
-    {_ {__   {__     {_ __     {__    {__     {_ __     
-    {__ {__  {__    {_  {__     {__          {_  {__    
-    {__  {__ {__   {__   {__      {__       {__   {__   
-    {__   {_ {__  {______ {__        {__   {______ {__  
-    {__    {_ __ {__       {__ {__    {__ {__       {__ 
-    {__      {__{__         {__  {__ __  {__         {__
-                    Mission Control API`;
   await next();
+  const time = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`);
+});
+
+app.use(async (ctx, next) => {
+  const startTime = Date.now();
+  await next();
+  const delta = Date.now() - startTime;
+  ctx.response.headers.set("X-Response-Time", `${delta}ms`);
+});
+
+app.use(api.routes());
+app.use(api.allowedMethods());
+
+app.use(async (ctx) => {
+  const filePath = ctx.request.url.pathname;
+  const fileWhitelist = [
+    "/index.html",
+    "/js/script.js",
+    "/css/style.css",
+  ];
+  if (fileWhitelist.includes(filePath)) {
+    await send(ctx, filePath, {
+      root: `${Deno.cwd()}/public`,
+    });
+  }
 });
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  app.listen({ port: PORT });
+  await app.listen({ port: PORT });
 }
